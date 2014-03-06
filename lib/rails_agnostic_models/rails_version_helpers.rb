8d9d4d096111e821615b66c548b90c919a37df2e
BRADLEY TEMPLE <bradley.temple@jpmchase.com>
2014-03-06 08:57:23 -0500

    spinning out rails version helpers

---
+++ lib/rails_agnostic_models/rails_version_helpers.rb
@@ -0,0 +1,36 @@
+module RailsAgnosticModels
+  module RailsVersionHelpers
+    def self.included(klass)
+      klass.extend(ClassMethods)
+    end
+    module ClassMethods
+      def rails_2?
+        (defined? Rails) && (Rails::VERSION::MAJOR == 2)
+      end
+
+      def rails_3?
+        (defined? Rails) && (Rails::VERSION::MAJOR == 3)
+      end
+
+      def rails_4?
+        (defined? Rails) && (Rails::VERSION::MAJOR == 4)
+      end
+
+      # Takes a block that will only be executed in a Rails 2 Project
+      def rails_2(&block)
+        return nil unless rails_2?
+        yield
+      end
+
+      def rails_3(&block)
+        return nil unless rails_3?
+        yield
+      end
+
+      def rails_4(&block)
+        return nil unless rails_4?
+        yield
+      end
+    end
+  end
+end
