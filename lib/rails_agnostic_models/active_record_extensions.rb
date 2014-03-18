a42b14cb39fcadd4c09685fbb76c6bfe00289148
BRADLEY TEMPLE <bradley.temple@jpmchase.com>
2014-03-18 08:27:47 -0400

    Factoring out rails helpers

--- lib/rails_agnostic_models/active_record_extensions.rb
+++ lib/rails_agnostic_models/active_record_extensions.rb
@@ -1,52 +1,8 @@
+require_relative './rails_helpers'
 module RailsAgnosticModels
   module ActiveRecordExtensions
     module ClassMethods
       require 'ostruct'
-
-      # Checks if the host app is a Rails 2 app
-      def rails_2?
-        rails_loaded && (Rails::VERSION::MAJOR == 2)
-      end
-
-      # Checks if the host app is a Rails 3 app
-      def rails_3?
-        rails_loaded && (Rails::VERSION::MAJOR == 3)
-      end
-
-      # Checks if the host app is a Rails 4 app
-      def rails_4?
-        rails_loaded && (Rails::VERSION::MAJOR == 4)
-      end
-
-      # Checks if Rails is loaded
-      def rails_loaded
-        defined? Rails
-      end
-
-      # Takes a block that will only be executed in a Rails 2 Project
-      def rails_2(&block)
-        return nil unless rails_2?
-        yield
-      end
-
-      # Code to be executed insode of a Rails 3 app
-      def rails_3(&block)
-        return nil unless rails_3?
-        yield
-      end
-
-      # Code to be executed inside of a Rails 4 app
-      def rails_4(&block)
-        return nil unless rails_4?
-        yield
-      end
-
-      # Code to be executed only inside of a rails app 
-      def rails(&block)
-        return nil unless rails_loaded
-        yield
-      end
-
       # safely refer to constants that may not be defined. Useful in a gem that might get included in places that might not define EVERY active record model, such as
       # a satelite administration application. Note that you will still need to handle nil cases
       #
@@ -165,6 +121,7 @@ module RailsAgnosticModels
     end
     def self.included(klass)
       klass.extend(ClassMethods)
+      klass.extend(RailsAgnosticModels::RailsHelpers::ClassMethods)
     end
   end
   ActiveRecord::Base.send(:include, ActiveRecordExtensions)
